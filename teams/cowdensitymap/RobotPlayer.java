3b1cf37673391eb89a39908cf6f68539ed51622e
Ashley Wang <ashwang@mit.edu>
2014-01-07 16:37:47 -0500

    This is the initial version of the cow density map

---
+++ teams/cowdensitymap/RobotPlayer.java
@@ -0,0 +1,24 @@
+package cowdensitymap;
+
+//this code mostly focuses on finding the areas of greatest cow density. It does mostly nothing else. Not even move the soldiers in that direction.
+
+import battlecode.common.*;
+
+public class RobotPlayer{
+	
+	public static void run(RobotController rc){
+		//Setup the global variables
+		
+		//Establish directions
+		Direction allDirections[] = Direction.values();
+		for (int i = 0; i < allDirections.length; i ++) {
+			System.out.println(allDirections[i]);
+		}
+		
+		//Start the main method - I'll ignore the try catch things for now
+		while (true) {
+			
+		}
+		
+	}
+}
