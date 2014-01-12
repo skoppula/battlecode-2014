4a59380a71cc7cf52929e3daa3e872e17e36fe68
Skanda Koppula <skanda.koppula@gmail.com>
2014-01-12 00:57:47 -0500

    revising everything to use broadcasts: static memory doesn't work

--- teams/origrestructured/RobotPlayer.java
+++ teams/origrestructured/RobotPlayer.java
@@ -10,19 +10,35 @@ import battlecode.common.RobotType;
 
 public class RobotPlayer {
     
-	public static volatile boolean firstRun = true;
+	/*
+	 * A value of (-1) implies no writing to channels has occurred.
+	 * Channel 0: Type of robot a spawned robot should be
+	 * Channel 1: firstRun for HQ; establishes (-1)'s
+	 * Channel 2 - 25: robot id:occupation mapping. Encoded as XXXXX0Y where XXXXX is the robot ID and Y is robot occupation
+	 * 					Y = 0 DEFENDER, 1 ATTACKER, 2 PASTR, 3 NOISETOWER
+	 */
 	
-    public static void run(RobotController rc){
+	 public static void run(RobotController rc){
     	
     	try {
     		int id = rc.getRobot().getID();
         	RobotType type = rc.getType();
         	
-        	if(firstRun){
-        		firstRun = false;
+        	
+        	if(rc.readBroadcast(1)!=-1){
+        		//On first run, set all robot occupations to -1.
+        		for(int i = 0; i < 26; i++)
+        			rc.broadcast(i, -1);
+        		
         	} else {
-        		HQ.occupations.put(id, HQ.tempSpawnedType);
-        		Util.printHashMap(HQ.occupations);
+        		//Put ID and occupation into appropriate channel
+        		for(int i = 2; i < 26; i++){
+        			if(rc.readBroadcast(i)==-1) {
+        				rc.broadcast(i, 100*id+rc.readBroadcast(0));
+        				break;
+        			}
+        		}
+//        		HQ.occupations.put(id, HQ.tempSpawnedType);
         	}
         	
         	while(true) {
@@ -36,17 +52,19 @@ public class RobotPlayer {
         		else if(type == RobotType.NOISETOWER)
         			NOISE.maintainNoiseTower(rc);
         		
-        		else if(HQ.occupations.get(id) == HQ.types.PASTR)
-        			PASTR.runPastureCreator(rc);
-        		
-        		else if(HQ.occupations.get(id) == HQ.types.DEFENDER)
-        			COWBOY.runDefender(rc);
-        		
-        		else if(HQ.occupations.get(id) == HQ.types.ATTACKER)
-        			COWBOY.runAttacker(rc);
-        		
-        		else
-        			NOISE.runNoiseCreator(rc);
+        		else {
+        			for(int i = 2; i < 26; i++){
+        				int val = rc.readBroadcast(i);
+        				if((val-val%100)==id){
+        					switch(val%100){
+        						case 2: PASTR.runPastureCreator(rc); break;
+        						case 1: COWBOY.runAttacker(rc); break;
+        						case 0: COWBOY.runDefender(rc); break;
+        						case 3: NOISE.runNoiseCreator(rc);
+        					}
+        				}
+        			}
+        		}
 	
         		rc.yield();
             }
